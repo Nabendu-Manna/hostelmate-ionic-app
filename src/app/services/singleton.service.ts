593fd71e2e9e814797d40ef76f98e8ea3527731b
Nabendu Manna <69715068+Nabendu-Manna@users.noreply.github.com>
2022-04-30 13:47:19 +0530

    header

--- src/app/services/singleton.service.ts
+++ src/app/services/singleton.service.ts
@@ -9,7 +9,8 @@ export class SingletonService {
   public roomsApi: string;
 
   constructor() {
-    this.basePath = 'http://192.168.0.111:8000';
+    // this.basePath = 'http://192.168.0.111:8000';
+    this.basePath = 'http://127.0.0.1:8000';
     this.installedAppVersion = '0.1';
     this.roomsApi = "/hostel/room/";
   }
